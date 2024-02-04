5f99c61663f96e605d38deafcdb2831131131684
Please set this to local username <please_set_this_to_local_user_email@blah.blah>
2024-02-04 07:36:04 +0000

    fix download_script to init folder

--- download_script.sh
+++ download_script.sh
@@ -1,3 +1,4 @@
+mkdir -p dataset download_progress raw_file
 wget --no-check-certificate -O raw_file/ssv2.zip  https://box.nju.edu.cn/seafhttp/files/64bf9a11-7679-422c-bb81-313b6ab349ec/smsm_otam.zip -o download_progress/ssv2.log && unzip raw_file/ssv2.zip -d dataset -o >> download_progress/ssv2.log &
 wget --no-check-certificate -O raw_file/kinetics.zip  https://box.nju.edu.cn/seafhttp/files/ceac0d0a-846b-47dd-b24c-f118bd21a98d/kinetics_100.zip -o download_progress/kinetics.log && unzip raw_file/kinetics.zip -d dataset -o >> download_progress/kinetics.log &
 wget --no-check-certificate -O raw_file/ucf.rar  https://www.crcv.ucf.edu/data/UCF101/UCF101.rar -o download_progress/ucf.log && unrar x -o+ raw_file/ucf.rar -d dataset >> download_progress/ucf.log &
